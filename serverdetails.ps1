8bff0fce0e1d26976776fc7f49d647c5aaf133e4
r3adm3 <adrianjfarnell@gmail.com>
2015-10-17 23:56:00 +0100

    initial commit

---
+++ serverdetails.ps1
@@ -0,0 +1,22 @@
+
+$ip = Get-NetIPAddress | where {$_.InterfaceAlias -eq 'Ethernet' -and $_.AddressFamily -eq 'IPv4'} | select-object -expandproperty ipaddress
+ 
+$machinedomain = gwmi -class win32_computersystem | select -expandproperty domain
+
+$hostname = gwmi -class win32_computersystem | select -ExpandProperty name
+
+$ram = gwmi -class win32_computersystem | select -expandproperty totalphysicalmemory
+$ram = $ram / 1mb 
+
+$cpu = gwmi -class win32_processor | select -expandproperty Name
+
+$OSversion = gwmi -class win32_operatingsystem | select -expandproperty Caption
+
+write-host "*****************************************************"
+write-host "*               $hostname                           "
+write-host "*         Logged On User: $env:USERNAME               "
+write-host "*       $osversion                         "
+write-host "*      CPU: $cpu   "
+write-host "*             RAM: $ram"
+write-host "*      IP: $ip   Machine Domain: $machinedomain "
+write-host "*****************************************************"
